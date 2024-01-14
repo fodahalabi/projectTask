8937593188e1022bf1da55ffb3c9052f4d6810bc
fodahalabi <62176427+fodahalabi@users.noreply.github.com>
2024-01-14 21:58:35 +0200

    Update partTwo.sh

--- partTwo.sh
+++ partTwo.sh
@@ -1,38 +1,23 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Error: Please provide a valid number for the number of files."
-    exit 1
-fi
+N=$1
+directory=$2
 
-if ! [[ $1 =~ ^[0-9]+$ ]]; then
-    echo "Error: The provided argument is not a valid number."
-    exit 1
-fi
+generate_random_filename() {
+    echo $((RANDOM % 512)).txt
+}
 
-num_files=$1
-for ((i = 0; i < num_files; i++)); do
-    file_name="${i}.txt"
-    touch "$file_name"
-done
+generate_random_permission() {
+    echo $((RANDOM % 8))$((RANDOM % 8))$((RANDOM % 8))
+}
 
-echo "Generated $num_files files with names like 0.txt, 1.txt, etc."
+mkdir -p "$directory"
 
+for ((i=0; i<N; i++)); do
+    filename=$(generate_random_filename)
+    touch "$directory/$filename"
 
-for ((i = 0; i < $1; i++)); do
-    touch "${i}.txt"
-    chmod +rwx "${i}.txt"
-done
-
-for ((i = 0; i < $1; i++)); do
-    file_name="${i}.txt"
-    if [ -e "$file_name" ]; then
-        if [ -r "$file_name" ] && [ -w "$file_name" ] && [ -x "$file_name" ]; then
-            echo "File $file_name has read, write, and execute permissions."
-        else
-            echo "Error: File $file_name does not have the expected permissions."
-        fi
-    else
-        echo "Error: File $file_name does not exist."
-    fi
+    # Generate random permission and apply
+    perm=$(generate_random_permission)
+    chmod "$perm" "$directory/$filename"
 done
